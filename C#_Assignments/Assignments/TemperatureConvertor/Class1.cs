84945ade0ad8d6d22014f76a5a8ebdf286fe5433
Reshmapalla <reshmapalla2@gmail.com>
2024-07-24 15:29:41 +0530

    commit

---
+++ C#_Assignments/Assignments/TemperatureConvertor/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace TemperatureConvertor
+{
+    public class Temperature
+    {
+        public  double  FTC(double temp)
+        {
+            double C = (temp - 32) * (5.0 / 9.0);
+            return C;
+            
+        }
+        public double CTF(double temp)
+        {
+            double F = (9 / 5) * temp + 32;
+
+            return F;
+
+        }
+    }
+}
