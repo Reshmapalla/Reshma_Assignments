ce14a315dc596c69ba0b9e8db7b67433cf8ceb36
Reshmapalla <reshmapalla2@gmail.com>
2024-07-31 17:18:48 +0530

    committ

--- MVC assignments/Assignment_2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HandsOnEFDBFirst.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
