218f2cac27cc701d917b08f0d0b527ddd1b9ed8d
Reshmapalla <reshmapalla2@gmail.com>
2024-07-31 17:12:40 +0530

    commit

---
+++ MVC assignments/Assignment_2/Entities/VwEmployeeDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HandsOnEFDBFirst.Entities;
+
+public partial class VwEmployeeDetail
+{
+    public string EmpName { get; set; } = null!;
+
+    public int EmpId { get; set; }
+
+    public string? Designation { get; set; }
+}
