e709978019ede27e7020824415ece031adb9b10d
Reshmapalla <reshmapalla2@gmail.com>
2024-07-31 10:02:44 +0530

    commit

---
+++ MVC assignments/Assignment_1/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Assignment_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Assignment_1.Controllers
+{
+    public class MovieController : Controller
+    {
+        MovieRepository movieRepository;
+        public MovieController()
+        {
+            movieRepository = new MovieRepository();
+        }
+        public IActionResult Index()
+        {
+            var movies = movieRepository.GetAllMovies();
+            return View(movies);
+        }
+        public IActionResult Details(string name)
+        {
+            var movie = movieRepository.GetMovieByName(name);
+            return View(movie);
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Movie movie)
+        {
+            movieRepository.Add(movie);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ MVC assignments/Assignment_1/Models/MovieRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_1.Models
+{
+    public class MovieRepository
+    {
+        static List<Movie> movies= new List<Movie>();
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+        public Movie GetMovieByName(string name)
+        {
+            foreach(Movie item in movies)
+            {
+                if(item.Title.Equals(name))
+                    return item;
+            }
+            return null;
+        }
+        public void Add(Movie movie) 
+        { 
+            movies.Add(movie);
+        }
+    }
+}
