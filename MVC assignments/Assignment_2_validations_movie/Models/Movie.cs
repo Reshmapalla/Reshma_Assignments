00ab2a817f45ebb5fa73f3069bdc02a0da212fc1
Reshmapalla <reshmapalla2@gmail.com>
2024-08-02 08:31:31 +0530

    commited

---
+++ MVC assignments/Assignment_2_validations_movie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Assignment_1.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "Plese enter Id")]
+        public int MovieId {  get; set; }
+        [Required(ErrorMessage = "Plese enter Title")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Plese enter Actor")]
+        public string Actor { get; set; }
+        [Required(ErrorMessage = "Plese enter Language")]
+
+        public string Language { get; set; }
+        [Required(ErrorMessage = "Plese enter Director")]
+
+        public string Director { get; set; }
+    }
+}
