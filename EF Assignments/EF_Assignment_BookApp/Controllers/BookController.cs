92de9cc8b91292fcda942ef4cb294a4800c6a8f6
Reshmapalla <reshmapalla2@gmail.com>
2024-08-07 07:09:30 +0530

    commit

---
+++ EF Assignments/EF_Assignment_BookApp/Controllers/BookController.cs
@@ -0,0 +1,103 @@
+﻿using EF_Assignment_BookApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF_Assignment_BookApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookAppContext bookAppContext;
+        public BookController()
+        {
+           
+            bookAppContext = new BookAppContext();
+        }
+        [HttpGet]
+        public IActionResult GetAllbooks()
+        {
+            var books = bookAppContext.Books;
+            return View(books);
+        }
+       [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]       
+        public IActionResult Create(Book book)
+
+        {
+            if (ModelState.IsValid)
+            {
+                bookAppContext.Books.Add(book);
+                bookAppContext.SaveChanges();
+                return RedirectToAction("GetAllbooks");
+
+            }
+            else
+                return View();
+
+            
+            
+        }
+        [HttpGet]
+        public IActionResult GetBookByAuthor(string name)
+        {
+            var book=bookAppContext.Books.Where(n=>n.Author==name).ToList();
+            return View(book);
+        }
+        [HttpGet]
+        public IActionResult GetBookByLang(string lang)
+        {
+            var book= bookAppContext.Books.SingleOrDefault(n=>n.Language==lang);
+            return View(book);
+        }
+
+        [HttpGet]
+        public IActionResult GetBookByYear(int year)
+        {
+            var book= bookAppContext.Books.Where(b => b.Releasedate.Value.Year == year).ToList();
+            return View(book);
+        }
+
+        [HttpGet]   
+        public IActionResult Edit(int id)
+        {
+            var book = bookAppContext.Books.SingleOrDefault(n => n.BookId == id);
+            return View(book);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                bookAppContext.Books.Update(book);
+                bookAppContext.SaveChanges();
+                return RedirectToAction("GetAllBooks");
+            }
+            else
+                return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        { 
+            var book=bookAppContext.Books.SingleOrDefault(n => n.BookId == id);
+            return View(book);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            bookAppContext.Books.Remove(book);
+            bookAppContext.SaveChanges();
+            return RedirectToAction("GetAllBooks");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EF Assignments/EF_Assignment_BookApp/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_Assignment_BookApp.Entities;
+
+public partial class Book
+{
+    [Required(ErrorMessage ="please enter Id")]
+    public int BookId { get; set; }
+    [Required(ErrorMessage = "please enter name")]
+    public string? BookName { get; set; }
+    [Required(ErrorMessage = "please enter price")]
+
+    public int? Price { get; set; }
+    [Required(ErrorMessage = "please enter Author")]
+
+    public string? Author { get; set; }
+    [Required(ErrorMessage = "please enter language")]
+
+    public string? Language { get; set; }
+    [Required(ErrorMessage = "please enter Date")]
+
+    public DateOnly? Releasedate { get; set; }
+}
